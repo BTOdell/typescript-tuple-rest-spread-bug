8424485b4210bd9cd5641262c321938342c933d6
Bradley Odell <btodell@hotmail.com>
2019-11-17 03:21:13 -0800

    Implemented code to reproduce bug behavior.

---
+++ index.ts
@@ -0,0 +1,25 @@
+/**
+ * Uses 'conditional types' to produce a tuple (array) of argument types based on the input type T.
+ * Example:
+ *  - If T is an array, then the array itself is used as the argument types.
+ *  - If T is anything else, then a tuple (of size 1) containing the single type is produced.
+ */
+type Args<T> = T extends any[] ? T : [T];
+
+export class EventEmitter<E> {
+
+    public restConditional<K extends keyof E>(eventName: K, listener: (...args: Args<E[K]>) => any): this {
+
+        return this;
+    }
+
+    public restGenerics<K extends keyof E>(eventName: K, listener: (...args: [boolean, boolean]) => any): void {
+    }
+
+    public rest(listener: (...args: [boolean, boolean]) => any): void {
+    }
+
+    public normal(listener: (b: boolean, b2: boolean) => any): void {
+    }
+
+}
---
+++ test.ts
@@ -0,0 +1,38 @@
+import {EventEmitter} from "./index";
+
+interface Events {
+    multiArray: [boolean, boolean];
+    multiOptionalArray: [boolean, boolean?];
+}
+
+class Test<E extends Events = Events> {
+
+    /**
+     * If the second line is uncommented, all unexpected errors are resolved.
+     */
+    private readonly ee: EventEmitter<E> = new EventEmitter();
+    //private readonly ee: EventEmitter<Events> = new EventEmitter();
+
+    constructor() {
+        this.ee.normal(b => {}); // No error
+        this.ee.normal((b, b2) => {}); // No error
+        this.ee.normal((b, b2, b3) => {}); // Expected error
+
+        this.ee.rest(b => {}); // No error
+        this.ee.rest((b, b2) => {}); // No error
+        this.ee.rest((b, b2, b3) => {}); // Expected error
+
+        this.ee.restGenerics("multiArray", b => {}); // No error
+        this.ee.restGenerics("multiArray", (b, b2) => {}); // No error
+        this.ee.restGenerics("multiArray", (b, b2, b3) => {}); // Expected error
+
+        this.ee.restConditional("multiArray", b => {}); // Unexpected error
+        this.ee.restConditional("multiArray", (b, b2) => {}); // No error
+        this.ee.restConditional("multiArray", (b, b2, b3) => {}); // Expected error
+
+        this.ee.restConditional("multiOptionalArray", b => {}); // Unexpected error
+        this.ee.restConditional("multiOptionalArray", (b, b2?) => {}); // No error
+        this.ee.restConditional("multiOptionalArray", (b, b2) => {}); // Expected error
+    }
+
+}
